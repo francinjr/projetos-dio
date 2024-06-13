8121b0c0ca826a6b67bfd8860608f2fb2e75d27c
francinjr <francinaldomanoel135@gmail.com>
2024-06-13 20:50:10 -0300

    Adicionar DesafioControleFluxo

---
+++ trilha-java-basico/DesafioControleFluxo/src/controlefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package controlefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+}
