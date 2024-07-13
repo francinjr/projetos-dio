9fa099514366f4a49a6cc9698cbbd182ad509c02
francinjr <francinaldomanoel135@gmail.com>
2024-07-13 16:51:04 -0300

    Adicionar padroes de projetos com java e spring

---
+++ java-spring/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/*
+ * Projeto Spring Boot
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
--- java-spring/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/service/impl/EnderecoService.java
+++
@@ -1,2 +0,0 @@
-package one.digitalinnovation.gof.service.impl;public class EnderecoService {
-}
